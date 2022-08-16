acfbe7b7ac3f552c93deb64f7462f5af623f7384
LAPTOP-943GNEC5\anton <antonio.perez@thinkskink.com>
2022-08-16 03:04:14 -0500

    Primer commit

---
+++ routes/usuarios.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const { usuariosGet, 
+    usuariosPut, 
+    usuariosPost, 
+    usuariosDelete, 
+    usuariosPatch } = require('../controllers/usuarios');
+
+const router = Router();
+
+module.exports = router;
+
+router.get('/', usuariosGet);
+
+router.put('/:id', usuariosPut);
+
+router.post('/', usuariosPost);
+
+router.delete('/', usuariosDelete);
+
+router.patch('/', usuariosPatch);
